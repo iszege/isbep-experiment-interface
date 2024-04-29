3093a08f52784961be6e8b1f1ea9712436cf2c7c
Ilja <ilja.szegedi@gmail.com>
2024-04-29 19:09:20 +0200

    Tweak custom button

--- CustomControls/NavigationButton.cs
+++ CustomControls/NavigationButton.cs
@@ -63,5 +63,18 @@ namespace ExperimentInterface.CustomControls
             DependencyProperty.Register("NavigationUri", typeof(Uri), typeof(NavigationButton), new PropertyMetadata(null));
 
 
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(NavigationButton), new PropertyMetadata(null));
+
+
+
+
     }
 }

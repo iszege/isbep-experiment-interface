cd4ce92ec45fa14dff19cf6ae465231e8a9e9283
Ilja <ilja.szegedi@gmail.com>
2024-05-01 12:57:29 +0200

    add basic NavigationCard control

---
+++ CustomControls/NavigationCard.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExperimentInterface.CustomControls
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ExperimentInterface.CustomControls"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ExperimentInterface.CustomControls;assembly=ExperimentInterface.CustomControls"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:NavigationCard/>
+    ///
+    /// </summary>
+    public class NavigationCard : ButtonBase
+    {
+        static NavigationCard()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NavigationCard), new FrameworkPropertyMetadata(typeof(NavigationCard)));
+        }
+
+        public Uri NavigationUri
+        {
+            get { return (Uri)GetValue(NavigationUriProperty); }
+            set { SetValue(NavigationUriProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for NavigationUri.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NavigationUriProperty =
+            DependencyProperty.Register("NavigationUri", typeof(Uri), typeof(NavigationCard), new PropertyMetadata(null));
+
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CornerRadius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(NavigationCard), new PropertyMetadata(new CornerRadius(0)));
+
+    }
+}

748847d45e5b827fd65d4d1657d855c7afa44bc0
Ilja <ilja.szegedi@gmail.com>
2024-05-10 17:07:07 +0200

    Update TaskManager.cs

--- Backend/TaskManager.cs
+++ Backend/TaskManager.cs
@@ -16,14 +16,30 @@ namespace ExperimentInterface.Backend
         
         private List<Task> tasks = new List<Task>();
         private List<TaskResult> results = new List<TaskResult>();
+
+        internal int feedbackTasks { get; set; }
+        internal int feedbackInterval { get; set; }
+        internal int remainingTasks { get; private set; }
         
         internal TaskManager()
         {
+            GetRequiredTasks();
             LoadTasks();
         }
 
+        #region Setup methods
+
+        private void GetRequiredTasks()
+        {
+            feedbackTasks = 5;
+            feedbackInterval = 2;
+            remainingTasks = (1 + feedbackInterval) * feedbackTasks;
+        }
+
+        #endregion
+
         #region Loading and Saving
-        
+
         private void LoadTasks()
         {
             tasks = dataManager.Load();
@@ -39,17 +55,29 @@ namespace ExperimentInterface.Backend
 
         #region Generating and Presenting Tasks
 
-        internal Task GetNextTask()
+        /// <summary>
+        /// Returns the next task to be presented, or null if the interaction method is completed and no tasks remain.
+        /// </summary>
+        /// <returns>The <c>Task</c> to be completed, or <c>null</c> if no tasks remain</returns>
+        internal Task? GetNextTask()
         {
+            if (remainingTasks <= 0) return null;
             if (tasks.Count == 0) LoadTasks();
 
             Task task = tasks.First();
             task.instructions = GetTaskInstructions(task);
             tasks.RemoveAt(0);
+            remainingTasks--;
 
             return task;
         }
 
+        /// <summary>
+        /// Generates a set of instructions for the provided <c>Task</c>, 
+        /// currently contains a number and a basket to place that number of items into.
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns>A <c>string[]</c> containing two instructions</returns>
         private string[] GetTaskInstructions(Task task)
         {
             int amount = random.Next(1, task.amount + 1);
@@ -58,6 +86,15 @@ namespace ExperimentInterface.Backend
             string side = sides[random.Next(sides.Length)];
 
             return new string[] { amount.ToString(), side };
+        }
+
+        /// <summary>
+        /// Determines whether the next task to be completed should include a feedback task.
+        /// </summary>
+        /// <returns><c>True</c> if feedback should be completed, <c>False</c> otherwise</returns>
+        internal bool GetNextTaskType()
+        {
+            return (remainingTasks % (feedbackInterval + 1) == 0 ) ? true : false;
         }
 
         #endregion

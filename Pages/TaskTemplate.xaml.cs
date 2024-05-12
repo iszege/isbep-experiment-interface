811f53060105e51f69d7e803659593c1bb300f0d
Ilja <ilja.szegedi@gmail.com>
2024-05-12 16:26:26 +0200

    allow pages to load task data

--- Pages/TaskTemplate.xaml.cs
+++ Pages/TaskTemplate.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using ExperimentInterface.CustomControls;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ExperimentInterface.Pages
 {
@@ -23,10 +12,109 @@ namespace ExperimentInterface.Pages
     /// </summary>
     public partial class TaskTemplate : Page
     {
+        MainWindow? mainWindow = Application.Current.MainWindow as MainWindow;
+        Stopwatch stopwatch = new Stopwatch();
+
         public TaskTemplate()
         {
             InitializeComponent();
+            NextTask();
         }
+
+        #region Task Setup
+
+        /// <summary>
+        /// Loads and displays the next task to be completed and starts the data collection or signals when no tasks remain
+        /// </summary>
+        private void NextTask()
+        {
+            if (mainWindow != null)
+            {
+                // Load next task
+                Backend.Task? NextTask = mainWindow.session.taskManager.GetNextTask();
+                bool feedback = mainWindow.session.taskManager.GetNextTaskType();
+
+                // Populate the page or exit in case no new task was loaded
+                if (NextTask != null)
+                {
+                    PopulateFields(NextTask, feedback);
+                    stopwatch.Restart();
+                }
+                else
+                {
+                    Exit();
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Populates the relevant fields on the task page with data from the loaded task
+        /// </summary>
+        private void PopulateFields(Backend.Task task, bool feedback)
+        {
+            // Set text and images
+            ItemName.Text = task.name;
+            //ItemPicture.Source = new BitmapImage(GetImageUri(task.id));
+            TaskInstructions.Text = GetTaskInstructions(task.name, task.instructions, feedback);
+
+            // Toggle required controls
+            DefaultControls.Visibility = (feedback) ? Visibility.Collapsed : Visibility.Visible;
+            FeedbackControls.Visibility = (feedback) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Returns a relative URI to the image corresponding to the provided id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>A relative URI</returns>
+        private Uri GetImageUri(int id)
+        {
+            return new Uri($"/Images/Tasks/{id}", UriKind.Relative);
+        }
+
+        /// <summary>
+        /// Generates instructions for the current task based on its name and embedded instructions and on whether feedback is required
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="instructions"></param>
+        /// <param name="feedback"></param>
+        /// <returns>A <c>string</c> containing all instructions to be displayed</returns>
+        private string GetTaskInstructions(string name, string[]? instructions, bool feedback)
+        {
+            // Ensure missing instructions are dealt with
+            string amount = (instructions != null) ? instructions[0] : "1";
+            string side = (instructions != null) ? instructions[1] : "left";
+
+            // Determine if name should be plural
+            int numAmount = (Int32.TryParse(amount, out int parsedAmount)) ? parsedAmount : 1;
+            name = (numAmount > 1) ? $"{name}s" : name;
+
+            // Generate and return instructions
+            string taskInstruction = $"Place exactly {amount} {name} (as shown on screen) in the basket on the {side.ToLower()}.";
+            string inputInstruction = (feedback)
+                                      ? "Complete the feedback task described below to continue."
+                                      : "Press the spacebar or click the button below to continue.";
+
+            return taskInstruction + "\n\n" + inputInstruction;
+        }
+
+        /// <summary>
+        /// Called when all tasks for this interaction are completed, closes the task page
+        /// </summary>
+        private void Exit()
+        {
+            // TODO navigate to a new exit page or simply back to the landing page
+        }
+
+        #endregion
+
+        #region Data Collection
+
+
+
+        #endregion
+
+        #region Button Listeners
 
         private void OnNextTaskButtonClick(object sender, RoutedEventArgs e)
         {
@@ -42,5 +130,7 @@ namespace ExperimentInterface.Pages
             else
                 Trace.WriteLine("Send this item to the robot arm in the future");
         }
+
+        #endregion
     }
 }

04ecfca65765781e4925ed756e8a640568451d14
Ilja <ilja.szegedi@gmail.com>
2024-05-19 17:25:19 +0200

    set up debug pages for voice and gestures

---
+++ Pages/GestureDebug.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace ExperimentInterface.Pages
+{
+    /// <summary>
+    /// Interaction logic for GestureDebug.xaml
+    /// </summary>
+    public partial class GestureDebug : Page
+    {
+        public GestureDebug()
+        {
+            InitializeComponent();
+        }
+
+        private void OnBackButtonClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}

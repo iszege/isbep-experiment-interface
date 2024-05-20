d1a2c55cb0f4b8de1e09ffb528c2684a9911790d
Ilja <ilja.szegedi@gmail.com>
2024-05-20 12:13:22 +0200

    Revert "start rework gesture detection"
    This reverts commit 2937a2365b09e6ea2c086b9a69e21bfa7a240027.

--- Pages/GestureDebug.xaml.cs
+++ Pages/GestureDebug.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using ExperimentInterface.Backend.Interactions;
 using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
@@ -20,31 +18,21 @@ namespace ExperimentInterface.Pages
             InitializeComponent();
         }
 
-        private async void OnPageLoaded(object sender, RoutedEventArgs e)
+        private void OnPageLoaded(object sender, RoutedEventArgs e)
         {
-            await WaitForDraw();
-            gestureInteraction = new GestureInteraction();
-            gestureInteraction.Start();
-            GestureInteraction.FeedbackGiven += GestureInteraction_FeedbackGiven;
-            //ReadFeedback();
-        }
-
-        private void GestureInteraction_FeedbackGiven(string obj)
-        {
-            Trace.WriteLine(obj);
-        }
-
-        /// <summary>
-        /// Implements an asynchronous delay before starting the python process to ensure all UI is drawn.
-        /// </summary>
-        private async Task WaitForDraw()
-        {
-            await Task.Delay(10);
+            ReadFeedback();
+            // Output.Text = new GestureInteraction().pyResult;
+            // Output.Text = new GestureInteraction().isPickable;
         }
 
         private void OnBackButtonClick(object sender, RoutedEventArgs e)
         {
             NavigationService.GoBack();
+        }
+
+        private void ReadFeedback()
+        {
+            Output.Text = new GestureInteraction().pyResult;
         }
     }
 }
